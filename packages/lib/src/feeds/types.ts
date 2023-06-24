74dcb2aeccee0e260828b4607b848a56074afc45
amandesai01 <amandesai01@gmail.com>
2023-06-24 17:25:50 +0530

    chore: remove deprecated package: lib

--- packages/lib/src/feeds/types.ts
+++
@@ -1,8 +0,0 @@
-import { Account, Label, RecurringTransaction, Transaction } from '../lookups';
-
-export type ApplicationFeed = {
-  accounts: Collection<Account>;
-  transactions: Collection<Transaction>;
-  labels: Collection<Label>;
-  recurringTransactions: Collection<RecurringTransaction>;
-};
--- packages/lib/tests/account.e2e-spec.ts
+++
@@ -1,8 +0,0 @@
-// import { assets } from '../src';
-// import { Account } from '../src/lookups';
-
-// describe('Assets > Account', () => {
-//     it('should properly create account', () => {
-//         assets.accounts.
-//     })
-// })
