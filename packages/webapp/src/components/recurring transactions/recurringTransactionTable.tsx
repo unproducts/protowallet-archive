2327f453fd6e478a6238c0cbfb9ffaf8d8637093
Utsav-Unadkat <utsavunadkat99@gmail.com>
2023-06-28 01:21:46 +0530

    r-tranx: populating data

--- packages/webapp/src/components/recurring transactions/recurringTransactionTable.tsx
+++ packages/webapp/src/components/recurring transactions/recurringTransactionTable.tsx
@@ -5,108 +5,111 @@ function RecurringTransactionTable({
   // selectedItems
 }) {
 
-  const invoices = [
-    {
-      id: '0',
-      invoice: '#123567',
-      total: '$129.00',
-      status: 'Overdue',
-      customer: 'Dominik Lamakani',
-      issueddate: '22/07/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    },
-    {
-      id: '1',
-      invoice: '#779912',
-      total: '$59.00',
-      status: 'Paid',
-      customer: 'Mark Cameron',
-      issueddate: '19/07/2021',
-      paiddate: '20/07/2021',
-      type: 'Subscription',
-    },
-    {
-      id: '2',
-      invoice: '#889924',
-      total: '$89.00',
-      status: 'Paid',
-      customer: 'Sergio Gonnelli',
-      issueddate: '17/07/2021',
-      paiddate: '19/07/2021',
-      type: 'One-time',
-    },
-    {
-      id: '3',
-      invoice: '#897726',
-      total: '$129.00',
-      status: 'Due',
-      customer: 'Manuel Garbaya',
-      issueddate: '04/07/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    },
-    {
-      id: '4',
-      invoice: '#123567',
-      total: '$129.00',
-      status: 'Due',
-      customer: 'Cool Robot',
-      issueddate: '04/07/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    },
-    {
-      id: '5',
-      invoice: '#896644',
-      total: '$129.00',
-      status: 'Paid',
-      customer: 'Mark Cameron',
-      issueddate: '04/07/2021',
-      paiddate: '09/07/2021',
-      type: 'One-time',
-    },
-    {
-      id: '6',
-      invoice: '#136988',
-      total: '$69.00',
-      status: 'Paid',
-      customer: 'Glenn Thomas',
-      issueddate: '01/07/2021',
-      paiddate: '01/07/2021',
-      type: 'One-time',
-    },
-    {
-      id: '7',
-      invoice: '#442206',
-      total: '$129.00',
-      status: 'Overdue',
-      customer: 'Dominik Lamakani',
-      issueddate: '22/06/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    },
-    {
-      id: '8',
-      invoice: '#764321',
-      total: '$89.00',
-      status: 'Paid',
-      customer: 'Brian Halligan',
-      issueddate: '21/06/2021',
-      paiddate: '29/06/2021',
-      type: 'One-time',
-    },
-    {
-      id: '9',
-      invoice: '#908764',
-      total: '$129.00',
-      status: 'Due',
-      customer: 'Carolyn McNeail',
-      issueddate: '17/06/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    }
-  ];
+  const invoices = Array.from({ length: 10 }, (_, index) => ({
+    title: `Title ${index + 1}`,
+    account: `Account ${index + 1}`,
+    amount: `$${Math.floor(Math.random() * 10000)}`,
+    category: `Category ${index + 1}`,
+    recordType: Math.random() < 0.5 ? 'Income' : 'Expense',
+    startDate: `${Math.floor(Math.random() * 30) + 1}/07/2021`,
+    repeat: Math.random() < 0.5 ? 'on' : 'off',
+  }));
+  // const invoices = [
+  //   {
+  //     title: 'Salary',
+  //     account: 'ABC Bank',
+  //     amount: '$129,000',
+  //     category: 'Wage',
+  //     recordType: 'Income',
+  //     startDate: '22/07/2021',
+  //     repeat: 'on',
+  //   },
+  //   {
+  //     id: '1',
+  //     invoice: '#779912',
+  //     total: '$59.00',
+  //     status: 'Paid',
+  //     customer: 'Mark Cameron',
+  //     issueddate: '19/07/2021',
+  //     paiddate: '20/07/2021',
+  //   },
+  //   {
+  //     id: '2',
+  //     invoice: '#889924',
+  //     total: '$89.00',
+  //     status: 'Paid',
+  //     customer: 'Sergio Gonnelli',
+  //     issueddate: '17/07/2021',
+  //     paiddate: '19/07/2021',
+  //     type: 'One-time',
+  //   },
+  //   {
+  //     id: '3',
+  //     invoice: '#897726',
+  //     total: '$129.00',
+  //     status: 'Due',
+  //     customer: 'Manuel Garbaya',
+  //     issueddate: '04/07/2021',
+  //     paiddate: '-',
+  //   },
+  //   {
+  //     id: '4',
+  //     invoice: '#123567',
+  //     total: '$129.00',
+  //     status: 'Due',
+  //     customer: 'Cool Robot',
+  //     issueddate: '04/07/2021',
+  //     paiddate: '-',
+  //   },
+  //   {
+  //     id: '5',
+  //     invoice: '#896644',
+  //     total: '$129.00',
+  //     status: 'Paid',
+  //     customer: 'Mark Cameron',
+  //     issueddate: '04/07/2021',
+  //     paiddate: '09/07/2021',
+  //     type: 'One-time',
+  //   },
+  //   {
+  //     id: '6',
+  //     invoice: '#136988',
+  //     total: '$69.00',
+  //     status: 'Paid',
+  //     customer: 'Glenn Thomas',
+  //     issueddate: '01/07/2021',
+  //     paiddate: '01/07/2021',
+  //     type: 'One-time',
+  //   },
+  //   {
+  //     id: '7',
+  //     invoice: '#442206',
+  //     total: '$129.00',
+  //     status: 'Overdue',
+  //     customer: 'Dominik Lamakani',
+  //     issueddate: '22/06/2021',
+  //     paiddate: '-',
+  //   },
+  //   {
+  //     id: '8',
+  //     invoice: '#764321',
+  //     total: '$89.00',
+  //     status: 'Paid',
+  //     customer: 'Brian Halligan',
+  //     issueddate: '21/06/2021',
+  //     paiddate: '29/06/2021',
+  //     type: 'One-time',
+  //   },
+  //   {
+  //     id: '9',
+  //     invoice: '#908764',
+  //     total: '$129.00',
+  //     status: 'Due',
+  //     customer: 'Carolyn McNeail',
+  //     issueddate: '17/06/2021',
+  //     paiddate: '-',
+  //   }
+  // ];
 
   const [selectAll, setSelectAll] = useState(false);
   const [isCheck, setIsCheck] = useState<any>([]);
@@ -161,25 +164,25 @@ function RecurringTransactionTable({
                   </div>
                 </th>
                 <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Invoice</div>
+                  <div className="font-semibold text-left">Title</div>
                 </th>
                 <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Total</div>
+                  <div className="font-semibold text-left">Account</div>
                 </th>
                 <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Status</div>
+                  <div className="font-semibold text-left">Amount</div>
                 </th>
                 <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Customer</div>
+                  <div className="font-semibold text-left">Category</div>
                 </th>
                 <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Issued on</div>
+                  <div className="font-semibold text-left">Record Type</div>
                 </th>
                 <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Paid on</div>
+                  <div className="font-semibold text-left">Start Date</div>
                 </th>
                 <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Type</div>
+                  <div className="font-semibold text-left">Repeat</div>
                 </th>
                 <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                   <div className="font-semibold text-left">Actions</div>
@@ -193,14 +196,14 @@ function RecurringTransactionTable({
                   return (
                     <RecurringTransactionTableItem
                       key={invoice.id}
-                      id={invoice.id}
-                      invoice={invoice.invoice}
-                      total={invoice.total}
-                      status={invoice.status}
-                      customer={invoice.customer}
-                      issueddate={invoice.issueddate}
-                      paiddate={invoice.paiddate}
-                      type={invoice.type}
+                      id={invoice.title}
+                      invoice={invoice.account}
+                      total={invoice.account}
+                      status={invoice.amount}
+                      customer={invoice.category}
+                      issueddate={invoice.recordType}
+                      paiddate={invoice.startDate}
+                      type={invoice.repeat}
                       handleClick={handleClick}
                       isChecked={isCheck.includes(invoice.id)}
                     />
--- packages/webapp/src/components/recurring transactions/recurringTransactions.tsx
+++ packages/webapp/src/components/recurring transactions/recurringTransactions.tsx
@@ -3,49 +3,24 @@ import RecurringTransactionModal from './recurringTransactionModal';
 import RecurringTransactionTable from './recurringTransactionTable';
 
 function RecurringTransactions() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const items2 = [
-    {
-      id: '0',
-      customer: 'Mark Cameron',
-      email: 'mark.cameron@app.com',
-      location: '🇬🇧 London, UK',
-      date: '22/01/2021',
-      amount: '+249.88',
-      descriptionTitle: 'Excepteur sint occaecat cupidatat.',
-      descriptionBody:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis. Ut enim ad minim veniam quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      id: '0',
-      customer: 'Mark Cameron',
-      email: 'mark.cameron@app.com',
-      location: '🇬🇧 London, UK',
-      date: '22/01/2021',
-      amount: '+249.88',
-      descriptionTitle: 'Excepteur sint occaecat cupidatat.',
-      descriptionBody:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis. Ut enim ad minim veniam quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-  ];
   return (
-      <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-        {/* Page header */}
-        <div className="sm:flex sm:justify-between sm:items-center mb-5">
-          {/* Left: Title */}
-          <div className="mb-4 sm:mb-0">
-            <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">Templates ✨</h1>
-          </div>
-
-          {/* Add card button */}
-          <RecurringTransactionModal />
+    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+      {/* Page header */}
+      <div className="sm:flex sm:justify-between sm:items-center mb-5">
+        {/* Left: Title */}
+        <div className="mb-4 sm:mb-0">
+          <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">Templates ✨</h1>
         </div>
 
-        {/* Categories cards */}
-        <div className="space-y-2">
-          <RecurringTransactionTable />
-        </div>
+        {/* Add card button */}
+        <RecurringTransactionModal />
       </div>
+
+      {/* Categories cards */}
+      <div className="space-y-2">
+        <RecurringTransactionTable />
+      </div>
+    </div>
   );
 }
 
